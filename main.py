a99af595a66884a4100bcf65bf8f50d089a3054d
Ramonnn <ramon.eliens@gmail.com>
2023-06-05 14:01:28 +0200

    added gitignore

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id):
-    post = find_post(int(id))
+def get_post(id: int):
+    post = find_post(id)
     return {"post_details": post}
