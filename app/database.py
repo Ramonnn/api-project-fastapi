6b0036d69b3d15235b97a54fbb1dfe81de9b5117
Ramonnn <ramon.eliens@gmail.com>
2023-06-13 14:45:54 +0200

    Added environmental vars

--- app/database.py
+++ app/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os.path import expanduser
 from urllib import parse
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 with open(expanduser('~/.pgpass'), 'r') as f:
     host, port, database, user, password = f.read().split(':')
@@ -20,4 +23,17 @@ def get_db():
     try:                 
         yield db         
     finally:             
-        db.close()       
+        db.close()      
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                                 cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)        
+
+
--- app/main.py
+++ app/main.py
@@ -1,37 +1,10 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-my_posts = [{"title": "favorite hobbies", "content": "I like videogames", "id": 1}, {
-    "title": "favorite foods", "content": "I like pizza", "id": 2}]
-
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)        
-
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
 
 app.include_router(post.router)
 app.include_router(user.router)
